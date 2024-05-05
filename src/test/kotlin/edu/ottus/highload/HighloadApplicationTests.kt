acdebc15fdee6a7c94cd0ef34e2654f2eabdcf24
Рудов Артем <arudov@inno.tech>
2024-05-05 17:42:50 +0300

    initail project

---
+++ src/test/kotlin/edu/ottus/highload/HighloadApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.ottus.highload
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HighloadApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
